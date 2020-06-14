1f75fac75fed6854a548ad29d652250f19086fea
Juyoung Lee <ljy070100@gmail.com>
2020-06-14 20:16:50 +0900

    회원가입 버튼 이벤트

--- app/src/main/java/kr/co/namu/loginpractice/MainActivity.kt
+++ app/src/main/java/kr/co/namu/loginpractice/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.loginpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,9 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        로그인 버튼이 눌리면 => 실행해줄 내용을 { } 안에 작성하자.
         loginBtn.setOnClickListener {
-//            코드에 남기는 메모
-//            주석
             Toast.makeText(this, "로그인 버튼이 눌림", Toast.LENGTH_SHORT).show()
+            Log.d("메인화면", "로그인 버튼이 눌림")
+        }
+
+//        회원가입 버튼이 눌리면
+        signupBtn.setOnClickListener {
+            Toast.makeText(this, "회원가입 버튼이 눌림", Toast.LENGTH_SHORT).show()
+            Log.d("메인화면", "회원가입 버튼이 눌림")
         }
 
 
